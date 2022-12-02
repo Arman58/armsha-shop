6ff241fe50f2dea7ec87d6d6fa73bc34838a69e5
Arman58 <Shaqaryan99@bk.ru>
2022-12-03 02:53:28 +0400

    asd

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
-import { Tab } from "@headlessui/react";
-import type { GetServerSideProps } from "next";
+import {Tab} from "@headlessui/react";
+import type {GetServerSideProps} from "next";
 import Head from "next/head";
 import Basket from "../components/Basket";
 import Header from "../components/Header";
 import Landing from "../components/Landing";
 import Product from "../components/Product";
-import { fetchCategories } from "../utils/fetchCategories";
-import { fetchProducts } from "../utils/fetchProducts";
-import { getSession } from "next-auth/react";
-import type { Session } from "next-auth";
+import {fetchCategories} from "../utils/fetchCategories";
+import {fetchProducts} from "../utils/fetchProducts";
+import {getSession} from "next-auth/react";
+import type {Session} from "next-auth";
 
 interface Props {
     categories: Category[];
@@ -16,28 +16,28 @@ interface Props {
     session: Session | null;
 }
 
-const Home = ({ categories, products }: Props) => {
+const Home = ({categories, products}: Props) => {
     console.log(products);
 
     const showProducts = (category: number) => {
         return products
-            .filter((product) => product.category._ref === categories[category]._id)
-            .map((product) => <Product product={product} key={product._id} />); // filter products by category
+            .filter((product) => product.category?._ref === categories[category]._id)
+            .map((product) => <Product product={product} key={product._id}/>); // filter products by category
     };
 
     return (
         <div className="">
             <Head>
                 <title>Apple Redesign</title>
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href="/favicon.ico"/>
             </Head>
 
-            <Header />
+            <Header/>
 
-            <Basket />
+            <Basket/>
 
             <main className="relative h-[200vh] bg-[#E7ECEE]">
-                <Landing />
+                <Landing/>
             </main>
             <section className="relative z-40 -mt-[100vh] min-h-screen bg-[#1B1B1B]">
                 <div className="space-y-10 py-16">
@@ -51,7 +51,7 @@ const Home = ({ categories, products }: Props) => {
                                 <Tab
                                     key={category._id}
                                     id={category._id}
-                                    className={({ selected }) =>
+                                    className={({selected}) =>
                                         `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
                                             selected
                                                 ? "borderGradient bg-[#35383C] text-white"
